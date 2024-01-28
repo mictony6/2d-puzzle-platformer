458b1c0d84b8ede111fd0fdd273c50983a696dbe
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2024-01-29 02:04:37 +0800

    Update player position and camera behavior

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,5 +1,9 @@
 extends Camera2D
-const max_h_offset : int = 50
+const max_h_offset : int = 16
+
+var follow_player_y = true
+const follow_offset = 15
+
 @export var player : Player
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,9 +12,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if player.velocity.normalized().x != 0:
-		offset.x =  lerpf(offset.x, player.velocity.normalized().x * max_h_offset, delta)
-	#else:
-		#offset.x =  lerpf(offset.x, 0 * max_h_offset, delta)
-		#offset.y =  lerpf(offset.y, player.velocity.normalized().y * max_h_offset, delta)
+	var top_threshold : float = get_screen_center_position().y -25
+	var bottom_threshold : float = get_screen_center_position().y + 60
+	var players_direction = player.velocity.normalized()
+	var x_factor = 1 if players_direction.x > 0 else -1  
+	if players_direction.x != 0:
+		offset = offset.move_toward( Vector2(x_factor* max_h_offset,offset.y), 75*delta)
 	position.x = player.position.x
+	
+	if player.position.y > bottom_threshold or player.position.y < top_threshold:
+		follow_player_y = true
+		
+	if follow_player_y:
+		#position = position.move_toward(Vector2(position.x,\
+		 #player.position.y-follow_offset), 50*delta)
+		position = position.lerp(Vector2(position.x, player.position.y-follow_offset), delta)
+		if abs(position.y - (player.position.y-follow_offset)) < 24:
+			follow_player_y = false
+	
+
+	
